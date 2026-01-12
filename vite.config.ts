de950b674a3d5076956b98802ac64163dad18c7f
Bruno Riwerson Silva <riwerson@gmail.com>
2026-01-12 16:10:06 -0300

    feat: prepare pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/spent-a-lot/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: [
+        'icons/spent-a-lot.png',
+        'icons/money-moving.png',
+        'icons/wallet.png',
+      ],
+
+      manifest: {
+        name: 'Spent-A-Lot',
+        short_name: 'Spent-A-Lot',
+        description: 'Planilha de gastos, mas não é planilha! ^^',
+        theme_color: '#1f2933',
+        background_color: '#0f172a',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+
+        icons: [
+          {
+            src: '/icons/spent-a-lot.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/spent-a-lot.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/spent-a-lot.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
